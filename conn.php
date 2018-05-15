8634a972a25abef54ba2d54f1c95df2a978de828
kishan-shetty <kishan-shetty@users.noreply.github.com>
2018-05-15 19:36:12 +0530

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+$db_host='localhost';
+$db_user='admin';
+$db_password='haha123';
+$db_name='pharma';
+$dbc=mysqli_connect($db_host,$db_user,$db_password,$db_name) or die ('Error connecting to mysql server');
+?>
